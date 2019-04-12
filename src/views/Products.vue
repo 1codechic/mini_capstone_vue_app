945207ee951a9cbaba203735aea1e8a7c1e62453
Antiffia McElrath <1codechic@devchics.com>
2019-04-11 22:43:20 -0400

    Products Page: added vue2filter and fadein/fadeout animation

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,37 +1,64 @@
 <template>
   <div class="root">
     <p>This is the Product Page</p>
-    <div v-for="product in products">
-      <p>{{ product.name}} </p>
-      <!-- <button v-on:click="toggleInfo(product)">Show more Info</button>   -->
-      <router-link v-bind:to="'/products/' + product.id">See more info</router-link>
-      <hr>
-    </div>
+
+    <input type="text" v-model="titleFilter" list="titles" />
+    <datalist id="name">
+      <option v-for="product in products">{{ product.name }}</option>
+    </datalist>
+
+    <button v-on:click="setSortAttribute('name')">Sort by name</button>
+    <button v-on:click="setSortAttribute('price')">Sort by price</button>
+
+    <transition-group appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
+      <div v-for="product in orderBy(products, sortAttribute, sortAsc)" v-bind:key="product.id">
+        <p>{{ product.name }}</p>
+        <p>{{ product.price }}</p>
+
+        <router-link v-bind:to="'/products/' + product.id">See more info</router-link>
+        <hr />
+      </div>
+    </transition-group>
   </div>
 </template>
 
 <style>
-  img{
-    width: 90px;
-  }
+img {
+  width: 90px;
+}
 </style>
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       message: "Welcome to Vue.js!",
       products: [],
+      sortAttribute: "name",
+      nameFilter: "",
+      sortAsc: 1,
       currentProduct: {}
     };
   },
   created: function() {
     axios.get("/api/products").then(response => {
       this.products = response.data;
-    })
+    });
   },
-  methods: {}
+  methods: {
+    setSortAttribute: function(attribute) {
+      console.log(attribute);
+      this.sortAttribute = attribute;
+      if (this.sortAsc === 1) {
+        this.sortAsc = -1;
+      } else {
+        this.sortAsc = 1;
+      }
+    }
+  }
 };
 </script>
